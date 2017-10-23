8a4614ab898898dbf1440bab5dd28c63b2b55c0c
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-10-22 20:13:10 -0500

    Preliminary work on Exercise 3

---
+++ CreatePosts.php
@@ -0,0 +1,51 @@
+<!-- Author: Taylor Walenczyk -->
+
+<html>
+<body>
+
+	<?php
+		$user = $_POST["username"];
+		$post = $_POST{"post"];
+		$my_db = new mysqli("mysql.eecs.ku.edu", "twalenczyk", "P@\$\$word123", "twalenczyk");
+		if ($my_db->connect_errno) {
+	    		printf("Connect failed: %s\n", $my_db->connect_error);
+	    		exit();
+		}
+		
+		if ($user) {
+			/* User name must exist */
+			$query = "SELECT * FROM Users WHERE user_id ='" . $user . "'";
+			
+			if ($result = $my_db->query($query)) {
+		
+	   			if ($result->num_rows > 0) {
+					if ($post) {
+					
+						$insert = "INSERT INTO Posts (content, author_id) VALUES ('" . $post . "','" . $user . "')";
+						if ($my_db->query($insert)) {
+							$message = "Your post was successfully created!";
+						} else {
+							$message = $my_db->error;
+						}			
+					} else {
+						$message = "Your post cannot be empty.";
+					}
+				} else {
+					$message = "You must register this username before using this service.";
+				}
+
+	   			/* free result set */
+	    			$result->free();
+			}
+		} else {
+			$message = "You must enter a username to use this service.";
+		}
+		/* close connection */
+		$my_db->close();
+	?>
+
+</body>
+</html>
+
+<h3> <?php echo $message ?> </h3>
+<h3> <?php echo $user ?> </h3>
