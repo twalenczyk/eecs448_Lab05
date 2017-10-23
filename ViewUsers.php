d45efcfd9383178499c366f1745680b7f58cca3a
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-10-22 22:42:00 -0500

    Now displays all users in the table

--- ViewUsers.php
+++ ViewUsers.php
@@ -2,6 +2,32 @@
 
 <html>
 <body>
+	<?php
+		$users = array();
+		$my_db = new mysqli("mysql.eecs.ku.edu", "twalenczyk", "P@\$\$word123", "twalenczyk");
+		
+		if ($my_db->connect_errno) {
+    			printf("Connect failed: %s\n", $my_db->connect_error);
+    			exit();
+		}
+
+		$query = "SELECT * FROM Users";
+	?>
+
+	<table>
+		<thead>
+			<th>Name</th>
+		</thead>
+		<tbody>
+			<?php if ($result = $my_db->query($query)): ?>
+				<?php while ($row = $result->fetch_assoc()): ?>
+					<tr>
+						<td> <?php echo $row["user_id"] ?> </td>
+					</tr>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</tbody>
+	</table>
 
 </body>
 </html>
